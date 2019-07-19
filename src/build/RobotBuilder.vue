0252ce3032ee705dda5c17a14c0b2b49e2329db3
geneowak <teugeneowak@gmail.com>
2019-07-19 11:06:15 +0300

    Bind attributes to Data with v-bind

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,31 +2,31 @@
   <div>
     <div class="top-row">
       <div class="top part">
-        <img src="./images/head-big-eye.png" title="head" />
+        <img v-bind:src="avalaibleParts.heads[0].src" title="head" />
         <button class="prev-selector">&#9668;</button>
         <button class="next-selector">&#9658;</button>
       </div>
     </div>
     <div class="middle-row">
       <div class="left part">
-        <img src="./images/arm-articulated-claw.png" title="left arm" />
+        <img v-bind:src="avalaibleParts.arms[0].src" title="left arm" />
         <button class="prev-selector">&#9650;</button>
         <button class="next-selector">&#9660;</button>
       </div>
       <div class="center part">
-        <img src="./images/torso-flexible-gauged.png" title="left arm" />
+        <img v-bind:src="avalaibleParts.torsos[0].src" title="torso" />
         <button class="prev-selector">&#9668;</button>
         <button class="next-selector">&#9658;</button>
       </div>
       <div class="right part">
-        <img src="./images/arm-dual-claw.png" title="left arm" />
+        <img v-bind:src="avalaibleParts.arms[0].src" title="right arm" />
         <button class="prev-selector">&#9650;</button>
         <button class="next-selector">&#9660;</button>
       </div>
     </div>
     <div class="bottom-row">
       <div class="bottom part">
-        <img src="./images/base-single-wheel.png" title="left arm" />
+        <img v-bind:src="avalaibleParts.bases[0].src" title="base" />
         <button class="prev-selector">&#9668;</button>
         <button class="next-selector">&#9658;</button>
       </div>
@@ -35,8 +35,15 @@
 </template>
 
 <script>
+import avalaibleParts from '../data/parts';
+
 export default {
   name: 'RobotBuilder',
+  data() {
+    return {
+      avalaibleParts,
+    };
+  },
 };
 </script>
 
